657f4e1385306b82735687b92926d320d6664d07
billx311 <bilal.zagham3@gmail.com>
2026-07-02 14:37:05 +0500

    Implement countdown timer using Temporal API
    Fix syntax errors and implement countdown logic using Temporal API.

--- countdown.js
+++ countdown.js
@@ -1,1 +1,6 @@
-console.log "COUNTDOWN";
+//take the instant temporal, then take the ZonedDateTime, then use .until()
+//show the output using console.log
+const instant = Temporal.Now.Instant();
+const mun_day = Temporal.ZonedDateTime.From("2026-7-21T09:00:00[Asia/Pakistan]");
+const timer = instant.until(mun_day);
+console.log(timer);
