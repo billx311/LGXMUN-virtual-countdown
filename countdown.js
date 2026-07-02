ba0651f65c4270020d2df95b2ad7cb75d1b0da61
billx311 <bilal.zagham3@gmail.com>
2026-07-02 15:12:22 +0500

    Refactor countdown.js for Temporal API improvements
    Updated Temporal API usage for timezone conversion and fixed method calls.

--- countdown.js
+++ countdown.js
@@ -1,6 +1,7 @@
-//take the instant temporal, then take the ZonedDateTime, then use .until()
+//take the instant temporal, then convert it to our timezone, then take the ZonedDateTime, then use .until()
 //show the output using console.log
-const instant = Temporal.Now.Instant();
-const mun_day = Temporal.ZonedDateTime.From("2026-7-21T09:00:00[Asia/Pakistan]");
-const timer = instant.until(mun_day);
-console.log(timer);
+const instant = Temporal.Now.instant();
+const timezone = instant.toZonedDateTimeISO("Asia/Pakistan");
+const mun_day = Temporal.ZonedDateTime.from("2026-07-21T09:00:00[Asia/Pakistan]");
+const timer = timezone.until(mun_day);
+console.log(timer.toString());
